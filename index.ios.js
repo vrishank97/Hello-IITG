dbdfae5b537766446ced79fcd0fae76937fbc94f
Vrishank Bhardwaj <vrishank1997@gmail.com>
2017-07-03 14:33:52 +0530

    First commit

---
+++ index.ios.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import {
+  AppRegistry,
+  Text,
+  View,
+  Button,
+} from 'react-native';
+import { StackNavigator } from 'react-navigation';
+
+
+
+class ChatScreen extends React.Component {
+  static navigationOptions = {
+    title: 'Chat with Lucy',
+  };
+  render() {
+    return (
+      <View>
+        <Text>Chat with Lucy</Text>
+      </View>
+    );
+  }
+}
+
+class HomeScreen extends React.Component {
+  static navigationOptions = {
+    title: 'Welcome',
+  };
+  render() {
+    const { navigate } = this.props.navigation;
+    return (
+      <View>
+        <Text>Hello, Chat App!</Text>
+        <Button
+          onPress={() => navigate('Chat')}
+          title="Chat with Lucy"
+        />
+      </View>
+    );
+  }
+}
+
+
+
+const IITG = StackNavigator({
+  Home: { screen: HomeScreen },
+  Chat: { screen: ChatScreen },
+});
+
+AppRegistry.registerComponent('IITG', () => IITG);
