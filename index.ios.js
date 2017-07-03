f86d8cd0b801ac9d457b602237dec10bc3e9432c
Vrishank Bhardwaj <vrishank1997@gmail.com>
2017-07-03 14:52:22 +0530

    first commit

--- index.ios.js
+++ index.ios.js
@@ -1,50 +1,53 @@
-import React from 'react';
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
 import {
   AppRegistry,
+  StyleSheet,
   Text,
-  View,
-  Button,
+  View
 } from 'react-native';
-import { StackNavigator } from 'react-navigation';
 
-
-
-class ChatScreen extends React.Component {
-  static navigationOptions = {
-    title: 'Chat with Lucy',
-  };
+export default class IITG extends Component {
   render() {
     return (
-      <View>
-        <Text>Chat with Lucy</Text>
+      <View style={styles.container}>
+        <Text style={styles.welcome}>
+          Welcome to React Native!
+        </Text>
+        <Text style={styles.instructions}>
+          To get started, edit index.ios.js
+        </Text>
+        <Text style={styles.instructions}>
+          Press Cmd+R to reload,{'\n'}
+          Cmd+D or shake for dev menu
+        </Text>
       </View>
     );
   }
 }
 
-class HomeScreen extends React.Component {
-  static navigationOptions = {
-    title: 'Welcome',
-  };
-  render() {
-    const { navigate } = this.props.navigation;
-    return (
-      <View>
-        <Text>Hello, Chat App!</Text>
-        <Button
-          onPress={() => navigate('Chat')}
-          title="Chat with Lucy"
-        />
-      </View>
-    );
-  }
-}
-
-
-
-const IITG = StackNavigator({
-  Home: { screen: HomeScreen },
-  Chat: { screen: ChatScreen },
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
 });
 
 AppRegistry.registerComponent('IITG', () => IITG);
